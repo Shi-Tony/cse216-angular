944ae3ba406dd87412da09651575c16d803e0b85
Tony Shi <tony@TonysMacbook.local>
2023-08-09 00:58:55 -0400

    please work 3

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
-const express = require('express'); //Import the express dependency
-const app = express();              //Instantiate an express app, the main work horse of this server
-const port = 5002;                  //Save the port number where your server will be listening
+const express = require('express');
+const app = express();             
+const port = 5002;                 
 const someObject = require("./messages.json");
+const cors = require('cors');
 
-//Idiomatic expression in express to route and respond to a client request
+app.use(cors());
+
 app.get('/messages', (req, res) => {       
-    // res.sendFile('index.html', {root: __dirname});   
-    // res.setHeader("Access-Control-Allow-Origin", "*");
-    // res.setHeader("Access-Control-Allow-Methods", "OPTIONS, GET");
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "OPTIONS, GET");
   
     res.json(someObject);
 });
 
-app.listen(port, () => {            //server starts listening for any attempts from a client to connect at port: {port}
+app.listen(port, () => {           
     console.log(`Now listening on port ${port}`); 
 });
